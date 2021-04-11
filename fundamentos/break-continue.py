4bc1e34261571b4d0a8e908dad90a75ca3f2d8bf
Andres <afelro97@gmail.com>
2021-04-11 14:02:19 -0500

    Ciclos

---
+++ fundamentos/break-continue.py
@@ -0,0 +1,19 @@
+# #Imprimir solo las letras a
+for letra in "Holanda":
+    if letra == "a":
+        print(letra)
+        break
+else:
+    print("Fin ciclo for")
+
+print("Continua el programa")
+
+# #Imprimir solo numeros pares
+# #for i in range(6):
+# # if i%2 == 0:
+#  #     print(i)
+
+for i in range(6):
+    if i%2 != 0:
+        continue
+    print(i)
---
+++ fundamentos/ciclo-while.py
@@ -0,0 +1,13 @@
+#condicion = False
+#
+#while condicion:
+#   print("Ejecutando ciclo while")
+#else:
+#    print("Fin del ciclo while")
+
+i = 0
+while i < 11:
+    print(i)
+    i += 1
+else:
+    print("Fin ciclo while")
